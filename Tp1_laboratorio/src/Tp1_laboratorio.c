615e2da5e363f34d2cbc5e90cccab289524d24d7
Pablo Campos <pabloezequielc@gmail.com>
2020-09-10 22:20:46 -0300

    ejercicio hospital

---
+++ Tp1_laboratorio/src/Tp1_laboratorio.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Tp1_laboratorio.c
+ Author      : Pablo E. Campos DIV. 1.E
+ Version     :
+ Copyright   : tp1_laboratorio
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
